f53f4c749c273770fe47add179f9586203515e2c
Brian Hicks <brian@brianthicks.com>
2018-03-25 14:07:36 -0500

    first working version of mdToAst

---
+++ scripts/mdToAst.js
@@ -0,0 +1,61 @@
+const report = require("vfile-reporter");
+const unified = require("unified");
+const vfile = require("to-vfile");
+const yaml = require("js-yaml");
+
+// remove position unsafely (mutation)
+function removePropertiesUnsafe(props) {
+  function removeRecursively(node) {
+    props.forEach(prop => delete node[prop]);
+
+    if (node.children) {
+      node.children.forEach(removeRecursively);
+    }
+
+    return node;
+  }
+
+  return function(tree) {
+    return removeRecursively(tree);
+  };
+}
+
+// remove newline nodes unsafely (mutation)
+function removeNewlineNodesUnsafe() {
+  function removeRecursively(node) {
+    if (node.children) {
+      node.children = node.children
+        .filter(node => node.value !== "\n")
+        .map(removeRecursively);
+    }
+    return node;
+  }
+
+  return removeRecursively;
+}
+
+const parseWithFrontmatter = unified()
+  .use(require("remark-parse"))
+  .use(require("remark-frontmatter"), ["yaml"])
+  .use(function() {
+    // TODO: is there a less hacky way to do this? :\
+    var frontmatter = {};
+
+    this.Compiler = function(tree, file) {
+      return JSON.stringify({
+        frontMatter: frontmatter,
+        content: tree
+      });
+    };
+
+    return function(tree) {
+      frontmatter = yaml.safeLoad(tree.children[0].value);
+    };
+  })
+  .use(require("remark-rehype"))
+  .use(removePropertiesUnsafe, ["position"])
+  .use(removeNewlineNodesUnsafe)
+  .process(vfile.readSync("content/index.md"), function(err, file) {
+    console.error(report(err || file));
+    console.log(String(file));
+  });
