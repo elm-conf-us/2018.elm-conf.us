4a4eef1107b0c09621c7c9a70ee32413e7d0e614
Brian Hicks <brian@brianthicks.com>
2018-03-29 16:04:48 -0500

    add basic header

--- src/View.elm
+++ src/View.elm
@@ -3,6 +3,7 @@ module View exposing (view)
 import Html.Styled as Html exposing (Html)
 import Page exposing (Page)
 import Page.Content.View exposing (root)
+import Route
 import View.Elements as Elements
 
 
@@ -10,7 +11,14 @@ view : Result String Page -> Html msg
 view res =
     case res of
         Ok { content } ->
-            Elements.container [ root content ]
+            Elements.container
+                [ Elements.header
+                    [ ( "elm-conf", Route.index )
+                    , ( "About", Route.about )
+                    , ( "Speak at elm-conf", Route.speakAtElmConf )
+                    ]
+                , root content
+                ]
 
         Err err ->
             Html.text err
--- src/View/Elements.elm
+++ src/View/Elements.elm
@@ -1,10 +1,27 @@
-module View.Elements exposing (container)
+module View.Elements
+    exposing
+        ( container
+        , header
+        )
 
 import Css exposing (..)
-import Html.Styled as Html exposing (Html)
+import Html.Styled as Html exposing (Attribute, Html)
 import Html.Styled.Attributes as Attributes
+import Route exposing (Route)
 
 
 container : List (Html msg) -> Html msg
 container =
     Html.div [ Attributes.css [] ]
+
+
+header : List ( String, Route ) -> Html msg
+header routes =
+    Html.nav
+        []
+        (List.map (\( name, route ) -> headerLink name route) routes)
+
+
+headerLink : String -> Route -> Html msg
+headerLink string msg =
+    Html.text string
