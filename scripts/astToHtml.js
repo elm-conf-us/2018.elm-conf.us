44b46ec24514736ad24f919a36f05bbc2b3180a5
Brian Hicks <brian@brianthicks.com>
2018-04-01 14:10:20 -0500

    add fonts

--- scripts/astToHtml.js
+++ scripts/astToHtml.js
@@ -7,9 +7,11 @@ function main(input) {
   startScript = "Elm.Main.fullscreen(" + JSON.stringify(body) + ")";
 
   console.log(
-    '<html><head><link rel="stylesheet" href="/css/reset.css"><title>' +
-      frontMatter.title +
-      '</title></head><body><script src="/index.js"></script><script>' +
+    "<html><head>" +
+      '<link rel="stylesheet" href="/css/reset.css">' +
+      '<link href="https://fonts.googleapis.com/css?family=Josefin+Sans|Vollkorn" rel="stylesheet">' +
+      ("<title>" + frontMatter.title + "</title>") +
+      '</head><body><script src="/index.js"></script><script>' +
       startScript +
       "</script></body></html>"
   );
