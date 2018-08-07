b368fef32885c797713318961e6031060aedeedb
Brian Hicks <brian@brianthicks.com>
2018-08-06 22:18:17 -0500

    nah, that wasn't a good idea. timezones.

--- scripts/mdToAst.js
+++ scripts/mdToAst.js
@@ -43,11 +43,6 @@ const parseWithFrontmatter = unified()
     this.Compiler = function(tree, file) {
       frontmatter.source = file.history[0].replace("content/", "");
 
-      if (frontmatter.time) {
-        var date = new Date(frontmatter.time);
-        frontmatter.time = date.getTime() / 1000;
-      }
-
       return JSON.stringify({
         type: "page",
         frontMatter: frontmatter,
